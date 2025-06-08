c3a9f57a621e3403db43e398b15de4b0c684f1b3
Nitindeep Singh <nittin@Nitindeeps-MacBook-Air.local>
2025-06-08 14:02:45 +0530

    new

--- dms/src/libs/mongodb.js
+++ dms/src/libs/mongodb.js
@@ -13,14 +13,20 @@ if (!cached) {
 }
 
 export async function connectDB() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(m => m);
+    cached.promise = mongoose.connect(MONGODB_URI).then(mongooseInstance => {
+      console.log("MongoDB connected");
+      return mongooseInstance;
+    }).catch(err => {
+      console.error("MongoDB connection error:", err);
+      throw err;
+    });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
